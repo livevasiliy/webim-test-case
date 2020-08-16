593d21153890a399a7077642a299ea1f48933688
Vasiliy Pivovarov <vasiliy.mv.obzor@gmail.com>
2020-08-16 23:39:49 +0400

    Добавил класс для выбора активной сортировки.

--- src/components/Users/UserListHead.js
+++ src/components/Users/UserListHead.js
@@ -1,55 +1,64 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { sortUsernameBy } from '../../redux/actions'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
-import { ASC, DESC } from '../../redux/types'
-
+import {connect} from 'react-redux'
+import {sortUsernameBy} from '../../redux/actions'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAngleDown, faAngleUp} from '@fortawesome/free-solid-svg-icons'
+import {ASC, DESC} from '../../redux/types'
+import './Users.css'
 
 export const UserListHead = (props) => {
-  const onSort = sortDirection => {
-    props.sortUsernameBy(sortDirection)
-  }
+    const onSort = sortDirection => {
+        props.sortUsernameBy(sortDirection)
+    }
 
-  return (
-    <thead>
-    <tr>
-      {
-        props.users
-          .slice(0, 1)
-          .map(user => Object
-            .keys(user)
-            .map(key => (
-                <th key={key}>
-                  <span className={'mr-2'}>{key}</span>
-                  {key === 'id' ? (
-                    <>
-                      <FontAwesomeIcon
-                        className={"mx-2"}
-                        icon={faAngleUp}
-                        onClick={() => key === 'id'
-                          ? onSort(ASC)
-                          : null}
-                      />
-                      <FontAwesomeIcon
-                        icon={faAngleDown}
-                        onClick={() => key === 'id'
-                          ? onSort(DESC)
-                          : null}
-                      />
-                    </>
-                  ) : null}
-                </th>
-              ),
-            ),
-          )
-      }
-      <th>Actions</th>
-    </tr>
-    </thead>
-  )
+    return (
+        <thead>
+        <tr>
+            {
+                props.users
+                    .slice(0, 1)
+                    .map(user => Object
+                        .keys(user)
+                        .map(key => (
+                                <th key={key}>
+                                    <span className={'mr-2'}>{key}</span>
+                                    {key === 'id' ? (
+                                        <>
+                                            <FontAwesomeIcon
+                                                className={
+                                                    props.sortDirection === ASC ? 'active mx-2' : ''
+                                                }
+                                                icon={faAngleUp}
+                                                onClick={() => key === 'id'
+                                                    ? onSort(ASC)
+                                                    : null}
+                                            />
+                                            <FontAwesomeIcon
+                                                className={props.sortDirection === DESC ? 'active mx-2' : ''}
+                                                icon={faAngleDown}
+                                                onClick={() => key === 'id'
+                                                    ? onSort(DESC)
+                                                    : null}
+                                            />
+                                        </>
+                                    ) : null}
+                                </th>
+                            ),
+                        ),
+                    )
+            }
+            <th>Actions</th>
+        </tr>
+        </thead>
+    )
 }
 
-export default connect(null, {
-  sortUsernameBy
+const mapStateToProps = state => {
+    return {
+        sortDirection: state.sortBy.sortType,
+    }
+}
+
+export default connect(mapStateToProps, {
+    sortUsernameBy
 })(UserListHead)
